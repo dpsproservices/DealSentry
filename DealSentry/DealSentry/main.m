eba097ba6416958b41ba1a921a9ced691d3c1847
Joseph Skarulis <jskarulis@gmail.com>
2015-10-29 17:45:39 -0400

    Xcode project initialization.

---
+++ DealSentry/DealSentry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DealSentry
+//
+//  Created by Joseph Skarulis on 10/29/15.
+//  Copyright © 2015 Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
